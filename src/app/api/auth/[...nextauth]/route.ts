c8baba657f22563d7c0d3324afe8a31895e3f53b
Trang Vu <Trang.Vu3@metropolia.fi>
2025-07-19 14:39:39 +0300

    apollo and graphql setup

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+// src/app/api/auth/[...nextauth]/route.ts
+
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
@@ -5,16 +7,15 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-export const authOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GitHubProvider({
-        clientId: process.env.GITHUB_ID!,
-        clientSecret: process.env.GITHUB_SECRET!,
-        }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-}
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+})
 
-const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
