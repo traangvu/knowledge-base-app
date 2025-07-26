b010b8619c044762c3613b9e62bef4eae9f365df
Trang Vu <Trang.Vu3@metropolia.fi>
2025-07-26 10:32:15 +0300

    add page at settings

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { signIn, useSession } from 'next-auth/react';
 import { useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
